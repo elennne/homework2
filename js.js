6c7c0fc67678688ddcb816019a5ad855030d3c7d
ელენე ჩანადირი <117665219+elennne@users.noreply.github.com>
2023-01-26 01:14:27 +0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,126 @@
+/* 4.1 
+let arr = ["სალომე", "გიორგი", "უშანგი"];
+for (let i = 0; i<arr.length; i++); {
+    document.write(arr[i]+ "</br>");
+} */
+
+
+/* 4.2
+let arr = ["a", "b", "c"];
+arr[3]="d";
+document.write(arr + "<br>");
+arr[1]="x";
+document.write(arr);*/
+
+/* 4.3
+let arr = [];
+arr.push('a', 'b', 'c');
+document.write (arr); */
+
+/*???????? 4.4
+
+let arr = [1, 2, 3, 4, 5];
+let key1 = 1;
+let key2 = 2; */
+
+
+/* 4.5 მასივის სიგრძე არ შეიცვალა
+let arr = ['a', 'b', 'c', 'd', 'e'];
+console.log(arr.length);
+delete arr[1];
+delete arr[3];
+console.log(arr); */
+
+
+
+//4.6 part1 ?????
+
+/* 4.6
+let arr = [11, 22, 33, 44, 55];
+console.log(arr[4]); */
+
+/* 4.7
+let arr = [10, 11, 12, 13, 14];
+console.log(arr.length); */
+
+/* 4.8
+let arr = [10, 11, 12, 13, 14];
+console.log(arr.length); */
+
+
+/*4.9
+let arr = [1,2,3,4];
+function getBlankArray () {
+    return arr = [];
+}
+getBlankArray();
+console.log(arr); */
+
+
+/* 4.10
+let components = [1,2,3,4,5];
+function getNumOfComponents () {
+    return console.log(components.length);
+}
+getNumOfComponents();*/
+
+//4.11???
+
+/* 4.12
+ let toys = ['ოვერბორდი', 'პაზლი', 'Lego'];
+function addBall () {
+    
+    toys[3]='ბურთი';//ოთხი ელემენტი იქნება ჯამში
+    return toys = ['ოვერბორდი', 'პაზლი', 'Lego']; //დააბრუნებს ძველ მასივს და იქნება სამი ელემენტი 
+}
+
+addBall ();
+console.log(toys); */
+
+/*  4.13
+let toys = ['ოვერბორდი', 'პაზლი', 'Lego'];
+let toy = 'Yo-yo';
+function addToy (toys, toy) {
+    toys.push('Yo-yo');
+}
+addToy (toys, toy);
+console.log(toys); */
+
+/* 4.14 
+let toys = ['ოვერბორდი', 'პაზლი', 'Lego', 'Yo-yo'];
+function getLastToy () {
+    return toys = 'Yo-yo';
+}
+getLastToy ();
+console.log(toys); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
